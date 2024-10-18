7b49207bcd5d6dc5e3c7b73110e60939d32a6829
samajika47 <samajikachakma47@gmail.com>
2024-10-18 18:51:53 +0600

    home screen work

--- lib/view/screen/dashboard/home_screen.dart
+++ lib/view/screen/dashboard/home_screen.dart
@@ -5,8 +5,39 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Center(child: Text ("Home screen")),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.qr_code_scanner_rounded)),
+                Expanded(
+                  child: Container(
+                    color: Colors.orange,
+                    child: Row(
+                      children: [
+                        Icon(Icons.camera_alt),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(child: TextField()),
+                        Container(color: Colors.orange,child: Text("Search"),)
+                  
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: (){}, icon:Icon( Icons.notifications_outlined)),
+
+              ],
+            ),
+            Center(child: Text("Home screen")),
+          ],
+        ),
+      ),
     );
   }
 }
