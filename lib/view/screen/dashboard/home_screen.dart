3d798f8722a0de03c7d9867e92b3406e1ca10ab3
samajika47 <samajikachakma47@gmail.com>
2024-10-22 11:18:29 +0600

    init

--- lib/view/screen/dashboard/home_screen.dart
+++ lib/view/screen/dashboard/home_screen.dart
@@ -9,32 +9,46 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         body: Column(
           children: [
+            SizedBox(height: 10,),
             Row(
               children: [
+                SizedBox(
+                  width: 10,
+                ),
                 IconButton(
                     onPressed: () {},
                     icon: Icon(Icons.qr_code_scanner_rounded)),
                 Expanded(
                   child: Container(
-                    color: Colors.orange,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(width: 1.5, color: Colors.orange),
+                        borderRadius: BorderRadius.circular(10)),
                     child: Row(
                       children: [
                         Icon(Icons.camera_alt),
                         SizedBox(
                           width: 10,
                         ),
-                        Expanded(child: TextField()),
-                        Container(color: Colors.orange,child: Text("Search"),)
-                  
+                        Expanded(
+                            child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "Search now", border: InputBorder.none),
+                        )),
+                        IconButton(onPressed:(){}, icon:Icon(Icons.search)),
+                      
                       ],
                     ),
                   ),
                 ),
-                IconButton(onPressed: (){}, icon:Icon( Icons.notifications_outlined)),
-
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.notifications_outlined)),
+                SizedBox(
+                  width: 10,
+                ),
               ],
             ),
-            Center(child: Text("Home screen")),
           ],
         ),
       ),
